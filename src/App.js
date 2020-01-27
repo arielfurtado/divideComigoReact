75fbbe47fddcfc50ffb54ddb66d71b125b256a42
Lucio Ariel <lucio.furtado@wooza.com.br>
2020-01-27 16:10:11 -0300

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from './components/header';
+
+import './reset.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <Header/>
   );
 }
 
